e08b3f6210ce25f04e39d7e5cd7cb802c99e9784
ezekielalene <ezkielmisgae@gmail.com>
2023-04-06 00:37:52 +0300

    User router addition

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersController.postNew);
+
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
