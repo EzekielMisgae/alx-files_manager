5fdd35041431e8b9a7822401c1300f47d9639eae
ezekielalene <ezkielmisgae@gmail.com>
2023-04-06 00:18:03 +0300

    API

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-// Importing required modules
-const express = require('express');
-const router = require('./routes/index');
+import express from 'express';
+import router from './routes/index';
 
-// Setting up the port to run the server
-const port = process.env.PORT || 5000;
+const port = parseInt(process.env.PORT, 10) || 5000;
 
-// Creating an express app
 const app = express();
 
-// Enabling JSON parsing
 app.use(express.json());
-
-// Using router for API routes
 app.use('/', router);
 
-// Starting the server and listening to the specified port
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`The server is running on port ${port}`);
 });
 
-// Exporting the app
-module.exports = app;
+export default app;
